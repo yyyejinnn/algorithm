c23f9d33d47bce94e4e00e51d816bac62a812a69
yyyejinnn <lyjin2007@gmail.com>
2024-05-20 22:00:46 +0900

    240520:

---
+++ Programmers/Level1/240520_K번째수.js
@@ -0,0 +1,31 @@
+function bubbleSort(array) {
+  const newArr = array.slice();
+
+  for (let i = 0; i < newArr.length; i++) {
+    for (let j = 0; j < newArr.length - i - 1; j++) {
+      if (newArr[j] > newArr[j + 1]) {
+        const temp = newArr[j];
+        newArr[j] = newArr[j + 1];
+        newArr[j + 1] = temp;
+      }
+    }
+  }
+
+  return newArr;
+}
+
+function solution(array, commands) {
+  var answer = [];
+
+  for (let i = 0; i < commands.length; i++) {
+    const cmd = commands[i];
+
+    const arr = array.slice(cmd[0] - 1, cmd[1]);
+
+    const sorted = bubbleSort(arr);
+
+    answer.push(sorted[cmd[2] - 1]);
+  }
+
+  return answer;
+}
