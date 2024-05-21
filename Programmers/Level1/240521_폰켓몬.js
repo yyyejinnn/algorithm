413aff71fa41eeb795da51bb83e2b600ad32eda2
yyyejinnn <lyjin2007@gmail.com>
2024-05-21 21:31:57 +0900

    240521: 폰켓몬

---
+++ Programmers/Level1/240521_폰켓몬.js
@@ -0,0 +1,11 @@
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=javascript
+ * 해시
+ */
+
+function solution(nums) {
+  const max = nums.length / 2;
+  const set = new Set(nums);
+
+  return set.size > max ? max : set.size;
+}
