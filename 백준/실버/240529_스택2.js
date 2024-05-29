7545cef3e4f4f48988bb1b3d1de9f99f67e6e2ed
yyyejinnn <lyjin2007@gmail.com>
2024-05-29 09:32:18 +0900

    240529: 스택2_(2)
    시간초과 해결

--- 백준/실버/240529_스택2.js
+++ 백준/실버/240529_스택2.js
@@ -22,30 +22,36 @@ rl.on('line', (line) => {
 });
 
 rl.on('close', () => {
+  let result = [];
+
   for (let o of orders) {
     const [order, x] = o;
 
     switch (order) {
-      case 1: {
+      case '1': {
         stack.push(x);
         break;
       }
-      case 2: {
-        console.log(stack.pop() ?? -1);
+      case '2': {
+        result.push(stack.pop() ?? -1);
         break;
       }
-      case 3: {
-        console.log(stack.length);
+      case '3': {
+        result.push(stack.length);
         break;
       }
-      case 4: {
-        console.log(Number(stack.length === 0));
+      case '4': {
+        result.push(Number(stack.length === 0));
         break;
       }
-      case 5: {
-        console.log(stack[stack.length - 1] ?? -1);
+      case '5': {
+        result.push(stack[stack.length - 1] ?? -1);
         break;
       }
     }
   }
+
+  // console -> 시간 증가 -> 최소한으로 사용하자
+  // https://www.acmicpc.net/board/view/128927
+  console.log(result.join('\n'));
 });
