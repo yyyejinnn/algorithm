1d6fd2508494d46badadb59bad09aa4ef36a4349
yyyejinnn <lyjin2007@gmail.com>
2024-05-30 14:07:41 +0900

    240530:

---
+++ 백준/실버/240530_요세푸스 문제.js
@@ -0,0 +1,132 @@
+/**
+ * https://www.acmicpc.net/problem/11866
+ * 원형 연결리스트
+ */
+
+function solution1() {
+  const readline = require('readline');
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  let n, k;
+
+  rl.on('line', (line) => {
+    [n, k] = line.split(' ').map(Number);
+  });
+
+  class Node {
+    constructor(data) {
+      this.data = data;
+    }
+  }
+
+  class CircleLinkedList {
+    constructor() {
+      this.head = null;
+      this.count = 0;
+    }
+
+    push(number) {
+      const node = new Node(number);
+
+      if (!this.head) {
+        this.head = node;
+      } else {
+        let current = this.head;
+
+        while (current.next !== this.head) {
+          current = current.next;
+        }
+
+        current.next = node;
+      }
+
+      node.next = this.head;
+      this.count++;
+    }
+
+    delete(k) {
+      let current = this.head;
+      let prev;
+
+      let deleteData;
+
+      if (k === 1) {
+        deleteData = this.head.data;
+        this.head = this.head.next;
+        this.count--;
+
+        return deleteData;
+      }
+
+      for (let i = 1; i < k; i++) {
+        prev = current;
+        current = current.next;
+      }
+
+      deleteData = current.data;
+
+      prev.next = current.next;
+      this.head = current.next;
+      this.count--;
+
+      return deleteData;
+    }
+
+    size() {
+      return this.count;
+    }
+  }
+
+  rl.on('close', () => {
+    const result = [];
+
+    const q = new CircleLinkedList();
+
+    for (let i = 1; i < n + 1; i++) {
+      q.push(i);
+    }
+
+    let size = q.size();
+
+    while (size > 0) {
+      result.push(q.delete(k));
+
+      size = q.size();
+    }
+
+    console.log(`<${result.join(', ')}>`);
+  });
+}
+
+/** shift 사용했을 때 - 큐 */
+
+function solution2() {
+  // 7, 3 가정
+  const arr = [1, 2, 3, 4, 5, 6, 7];
+  const k = 3;
+
+  const result = [];
+
+  let i = 1;
+
+  while (arr.length > 0) {
+    const target = arr.shift();
+
+    if (i % k === 0) {
+      // k의 배수일 경우
+      result.push(target);
+    } else {
+      arr.push(target);
+    }
+
+    i++;
+  }
+
+  console.log(`<${result.join(', ')}>`);
+}
+
+solution2();
---
+++ 백준/실버/240530_큐2.js
@@ -0,0 +1,126 @@
+/**
+ * https://www.acmicpc.net/problem/18258
+ * 큐
+ */
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+class Node {
+  constructor(data, prev = null, next = null) {
+    this.data = data;
+    this.prev = prev;
+    this.next = next;
+  }
+}
+
+// double-linked-list 사용
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.count = 0;
+  }
+
+  push(data) {
+    const node = new Node(data);
+
+    if (this.count === 0) {
+      this.head = node;
+    } else {
+      node.prev = this.tail;
+      this.tail.next = node;
+    }
+
+    this.tail = node;
+    this.count++;
+  }
+
+  pop() {
+    if (this.count === 0) {
+      return -1;
+    }
+
+    const target = this.head;
+
+    if (this.count === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = this.head.next;
+      this.head.prev = null;
+    }
+
+    this.count--;
+    return target?.data;
+  }
+
+  size() {
+    return this.count;
+  }
+
+  empty() {
+    return this.count === 0 ? 1 : 0;
+  }
+
+  front() {
+    return this.head?.data ?? -1;
+  }
+
+  back() {
+    return this.tail?.data ?? -1;
+  }
+}
+
+let orders = [];
+let count = 0;
+
+rl.on('line', (line) => {
+  if (count > 0) {
+    orders.push(line.split(' '));
+  }
+  count++;
+});
+
+rl.on('close', () => {
+  const result = [];
+
+  const queue = new Queue();
+
+  orders.forEach((o) => {
+    const [order, x] = o;
+
+    switch (order) {
+      case 'push': {
+        queue.push(x);
+        break;
+      }
+      case 'pop': {
+        result.push(queue.pop());
+        break;
+      }
+      case 'size': {
+        result.push(queue.size());
+        break;
+      }
+      case 'empty': {
+        result.push(queue.empty());
+        break;
+      }
+      case 'front': {
+        result.push(queue.front());
+        break;
+      }
+      case 'back': {
+        result.push(queue.back());
+        break;
+      }
+    }
+  });
+
+  console.log(result.join('\n'));
+});
