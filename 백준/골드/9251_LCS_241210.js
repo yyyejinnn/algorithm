0865fc235f61faa7ff3c70d6c5ba65747241beab
yyyejinnn <lyjin2007@gmail.com>
2024-12-10 13:13:01 +0900

    241210: LIS, LCS

---
+++ 백준/골드/9251_LCS_241210.js
@@ -0,0 +1,38 @@
+/**
+ * https://www.acmicpc.net/problem/9251
+ * LCS - dp
+ *
+ * https://velog.io/@emplam27/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EA%B7%B8%EB%A6%BC%EC%9C%BC%EB%A1%9C-%EC%95%8C%EC%95%84%EB%B3%B4%EB%8A%94-LCS-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-Longest-Common-Substring%EC%99%80-Longest-Common-Subsequence
+ */
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let inputs = [];
+
+rl.on('line', (line) => {
+  inputs.push(line);
+});
+
+rl.on('close', () => {
+  let [a, b] = inputs;
+  let [aLen, bLen] = [a.length, b.length];
+
+  let dp = Array.from({ length: aLen + 1 }, () => Array(bLen + 1).fill(0));
+
+  for (let i = 1; i <= aLen; i++) {
+    for (let j = 1; j <= bLen; j++) {
+      if (a[i - 1] === b[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+
+  console.log(dp[aLen][bLen]);
+});
---
+++ 백준/실버/11053_가장긴증가하는부분수열_241210.js
@@ -0,0 +1,33 @@
+/**
+ * https://www.acmicpc.net/problem/11053
+ * LIS - dp
+ */
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let inputs = [];
+
+rl.on('line', (line) => {
+  inputs.push(line.split(' ').map(Number));
+});
+
+rl.on('close', () => {
+  let [[n], nums] = inputs;
+
+  let dp = Array(n).fill(1);
+
+  for (let i = 1; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[j] < nums[i]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+  }
+
+  console.log(Math.max(...dp));
+});
