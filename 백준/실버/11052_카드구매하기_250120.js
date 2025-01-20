4d570a07421f841e09a418e48f96343679c85b3b
yyyejinnn <lyjin2007@gmail.com>
2025-01-20 16:11:49 +0900

    250120: dp

---
+++ 백준/실버/11052_카드구매하기_250120.js
@@ -0,0 +1,31 @@
+/**
+ * https://www.acmicpc.net/problem/11052
+ * dp
+ */
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let inputs = [];
+
+rl.on('line', (line) => {
+  inputs.push(line.split(' ').map(Number));
+});
+
+rl.on('close', () => {
+  let [[n], cards] = inputs;
+
+  let dp = Array(n + 1).fill(0);
+
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+      dp[i] = Math.max(dp[i], dp[i - j] + cards[j - 1]);
+    }
+  }
+
+  console.log(dp[n]);
+});
